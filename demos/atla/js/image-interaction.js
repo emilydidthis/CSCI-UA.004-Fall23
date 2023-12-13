4b180bb22b2d30fed4eeb4c831e61a61a02da06e
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2023-12-13 12:32:58 -0500

    final review shit

--- demos/atla/js/image-interaction.js
+++ demos/atla/js/image-interaction.js
@@ -1,36 +1,36 @@
-/*
-Changing Image Opacity
-*/
-
 let images = document.querySelectorAll('main > img');
-// console.log(images);
+let quote = document.querySelector('footer > p > em');
+let earth = document.getElementById("earth-blurb");
 
 function changeOpacity() {
-  images[0].style.opacity = '0.5'; // change opacity of each image
+  images[0].style.opacity = '0.5';
   images[1].style.opacity = '0.5';
   images[2].style.opacity = '0.5';
   images[3].style.opacity = '0.5';
 
-  this.style.opacity = '1'; // keep current image opaque
-  // in event handlers, "this" refers to the HTML element that received the event
-  // console.log(this);
+  this.style.opacity = '1'; 
 }
 
 function resetOpacity() {
-  images[0].style.opacity = '1'; // reset opacity of each image
+  images[0].style.opacity = '1'; 
   images[1].style.opacity = '1';
   images[2].style.opacity = '1';
   images[3].style.opacity = '1';
 }
 
-// event listeners for hover on
 images[0].addEventListener('pointerover', changeOpacity);
 images[1].addEventListener('pointerover', changeOpacity);
 images[2].addEventListener('pointerover', changeOpacity);
 images[3].addEventListener('pointerover', changeOpacity);
 
-// event listeners for hover off
 images[0].addEventListener('pointerout', resetOpacity);
 images[1].addEventListener('pointerout', resetOpacity);
+earth.style.display = "none";
 images[2].addEventListener('pointerout', resetOpacity);
 images[3].addEventListener('pointerout', resetOpacity);
+
+images[0].addEventListener('pointerover', ()=> quote.innerText = 'I will never turn my back on people who need me! — Katara');
+images[1].addEventListener('pointerover', ()=> quote.innerText = 'I am not Toph. I am Melon Lord! – Melon Lord');
+
+images[2].addEventListener('pointerover', ()=> quote.innerText = "No one can give you your honor. It's something you earn for yourself by choosing to do what's right. – Zuko");
+images[3].addEventListener('pointerover', ()=> quote.innerText = 'When we hit our lowest point, we are open to the greatest change – Aang');
